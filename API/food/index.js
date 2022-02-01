420cee4241aa4af8fc16e20f13005b167f3e56a7
priyanka323 <priyarankawat88160@gmail.com>
2022-02-01 06:43:43 -0800

    API

---
+++ API/food/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+import {FoodModel} from "../../database/allModel";
+
+const Router = express.Router();
+
+/*
+Route         /
+Des           get all the food based on particular resturant
+params        _id
+access        Public
+Method        get
+
+*/
+
+Router.get("/:_id", async(req,res)=> {
+  try {
+
+  } catch (error) {
+
+  } finally {
+
+  }
+})
---
+++ database/allModel.js
@@ -0,0 +1,17 @@
+import {FoodModel} from "./food";
+import {ImageModel} from "./image";
+import {MenuModel} from "./menu";
+import {OrderModel} from "./order";
+import {ResturantModel} from "./resturant";
+import {ReviewModel} from "./reviews";
+import {UserModel} from "./users";
+
+export {
+  FoodModel,
+  ImageModel,
+  MenuModel,
+  OrderModel,
+  ResturantModel,
+  ReviewModel,
+  UserModel
+};
