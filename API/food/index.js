8e69641650157cd5d8a5163ef5af3effe962f31a
priyanka323 <priyarankawat88160@gmail.com>
2022-02-01 23:59:57 -0800

    Food API

--- API/food/index.js
+++ API/food/index.js
@@ -15,10 +15,33 @@ Method        get
 
 Router.get("/:_id", async(req,res)=> {
   try {
+    const {_id} = req.params;
+    const foods = await FoodModel.find({resturant: _id});
+    return res.json({foods});
 
   } catch (error) {
-
-  } finally {
-
+    return res.status(500).json({error: error.meeage});
   }
-})
+});
+
+/*
+Route         /r
+Des           get all the food based on particular category
+params        category
+access        Public
+Method        get
+
+*/
+
+Router.get("/r/:category", async(req,res)=> {
+  try {
+    const {category} = req.params;
+    const foods = await FoodModel.find({
+      category: {$regex: category, $options: "i"}       //i-insenstive like uper lower case alphabet manage
+    });
+    return res.json({foods});
+  } catch (error) {
+    return res.status(500).json({error:error.message});
+  }
+});
+export default Router;
